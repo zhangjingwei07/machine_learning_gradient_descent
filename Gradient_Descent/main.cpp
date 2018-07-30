d2e46e0157a6f5975cfd974546d4f6cfd2e9023c
Jingwei Zhang <jwdaniel.zhang@mail.utoronto.ca>
2018-07-30 14:10:08 +0800

    Initial Commit

---
+++ Gradient_Descent/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gradient_Descent
+//
+//  Created by 张经纬 on 2018/7/30.
+//  Copyright © 2018 Jingwei Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
